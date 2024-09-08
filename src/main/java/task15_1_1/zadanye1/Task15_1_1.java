c1310f3be29fa91e7c146ab5258a795a08d00fd6
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-09-08 17:13:27 +0300

    240908 commit

--- src/main/java/task15_1_1/zadanye1/Task15_1_1.java
+++ src/main/java/task15_1_1/zadanye1/Task15_1_1.java
@@ -15,7 +15,18 @@ public class Task15_1_1 {
             """);
 
         System.out.println("""
-            Цитаты берём............
+                На веб-сайте: https://github.com/
+                Создан мой аккаунт (или profile), и в нём созданы мои репозитории.
+                Имя моего аккаунта (или логин):  Vladislav-NewJoined
+                Почта, указанная при регистрации: sozin.vladislav@mail.ru
+                Ссылка на мой аккаунт:\s
+                https://github.com/Vladislav-NewJoined
+                Пример моего репозитория для этого задания:\s
+                Имя репозитория:
+                n240221_Module1_Task1_Zadaniya1-16
+                Тип репозитория: Public
+                Ссылка на репозиторий:\s
+                https://github.com/Vladislav-NewJoined/n240221_Module1_Task1_Zadaniya1-16.git
             """);
 
 
